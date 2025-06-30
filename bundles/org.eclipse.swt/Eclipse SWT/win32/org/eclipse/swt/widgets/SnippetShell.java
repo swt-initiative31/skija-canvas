b4158c8130d63462e4ae76ae4b82746efec8796c
Denis Ungemach <denis.ungemach@sap.com>
2025-06-30 14:17:11 +0200

    Windows: Skija Dependencies. Minimal source code call.

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/SnippetShell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/SnippetShell.java
@@ -1,9 +1,17 @@
 package org.eclipse.swt.widgets;
 
+import io.github.humbleui.skija.*;
+
 public class SnippetShell {
 
 	public static void main(String[] arg) {
 		System.out.println("Test");
+
+		ImageInfo i = ImageInfo.makeN32(300, 300, ColorAlphaType.UNPREMUL);
+		try(Surface s = Surface.makeRaster(i)){
+
+		}
+
 	}
 
 

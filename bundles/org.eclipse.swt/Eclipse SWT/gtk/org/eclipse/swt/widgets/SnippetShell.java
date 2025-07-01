33c8e12193964955716ab3cef1ec5c4005161b37
Denis Ungemach <denis.ungemach@sap.com>
2025-07-01 09:15:27 +0200

    Skija: Dependencies for linux&mac Windows dependency adjustments. Linux dependency adjustments. Mac dependencies check. diable linux and mac check. snippetshell in windows. SnippetShell in Mac and Linux

---
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/SnippetShell.java
@@ -0,0 +1,17 @@
+package org.eclipse.swt.widgets;
+
+
+public class SnippetShell {
+
+	public static void main(String[] arg) {
+		System.out.println("Test");
+
+//		ImageInfo i = ImageInfo.makeN32(300, 300, ColorAlphaType.UNPREMUL);
+//		try(Surface s = Surface.makeRaster(i)){
+//
+//		}
+
+	}
+
+
+}

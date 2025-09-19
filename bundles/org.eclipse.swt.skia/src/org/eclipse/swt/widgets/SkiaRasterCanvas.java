6fc9fb8f5959dca5fbe3f827203f6418bdf2ea8f
az <az@LIT-CPS-Lab-001.jku.at>
2025-09-19 12:03:09 +0200

    Applied save clean-up actions to all swt skia files

--- bundles/org.eclipse.swt.skia/src/org/eclipse/swt/widgets/SkiaRasterCanvas.java
+++ bundles/org.eclipse.swt.skia/src/org/eclipse/swt/widgets/SkiaRasterCanvas.java
@@ -11,92 +11,100 @@
  *******************************************************************************/
 package org.eclipse.swt.widgets;
 
-import org.eclipse.swt.*;
-import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GCData;
+import org.eclipse.swt.graphics.GCExtension;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.graphics.SkiaGC;
 
-import io.github.humbleui.skija.*;
+import io.github.humbleui.skija.ColorAlphaType;
+import io.github.humbleui.skija.ImageInfo;
+import io.github.humbleui.skija.Surface;
 
 public class SkiaRasterCanvas extends RasterCanvas implements ISkiaCanvas {
 
-    private Surface surface;
+	private Surface surface;
 
-    public SkiaRasterCanvas(Composite parent, int style) {
-	super(parent, style);
-    }
-
-    @Override
-    public void doPaint(PaintEventSender s) {
-
-	if (surface == null)
-	    return;
-
-	// TODO get bg color from canvas...
-	surface.getCanvas().clear(0xFFFFFFFF);
-
-	Event event = new Event();
-	event.count = 1;
-
-	var size = getSize();
-
-	Rectangle eventBounds = new Rectangle(0, 0, size.x, size.y);
-	event.setBounds(eventBounds);
-	GCData data = new GCData();
-	data.device = this.getDisplay();
-	// critical for drawing without clearing
-	SkiaGC gc = new SkiaGC(this, SWT.None);
-
-
-	// here we need the skiaGC init not the gc init...
-	// Note: use GC#setClipping(x,y,width,height) because GC#setClipping(Rectangle)
-	// got broken by bug 446075
-//		gc.setClipping (eventBounds.x, eventBounds.y, eventBounds.width, eventBounds.height);
-	event.gc = new GCExtension(gc);
-	event.type = SWT.Paint;
-	s.sendPaintEvent(event);
-	gc.dispose();
-	event.gc = null;
-
-	surface.flush();
-
-    }
-
-    @Override
-    public void createSurface(long pointer, Point size, RasterImageInfo info) {
-
-	var s = getSize();
-	ColorAlphaType type = info.premule ? ColorAlphaType.PREMUL : ColorAlphaType.UNPREMUL;
-	ImageInfo imageInfo = ImageInfo.makeN32(size.x, size.y, type);
-
-	this.surface = Surface.makeRasterDirect(imageInfo, pointer, 4 * (size.x));
-
-	if (info.transform) {
-	    surface.getCanvas().scale(1, -1);
-	    surface.getCanvas().translate(0, -s.y);
+	public SkiaRasterCanvas(Composite parent, int style) {
+		super(parent, style);
 	}
 
-    }
+	@Override
+	public void doPaint(PaintEventSender s) {
 
-    @Override
-    void preResize(Event e) {
+		if (surface == null) {
+			return;
+		}
 
-	if (surface != null && !surface.isClosed())
-	    surface.close();
+		// TODO get bg color from canvas...
+		surface.getCanvas().clear(0xFFFFFFFF);
 
-    }
+		final Event event = new Event();
+		event.count = 1;
 
-    @Override
-    public long internal_new_GC(GCData data) {
-	return 0;
-    }
+		final var size = getSize();
 
-    @Override
-    public void internal_dispose_GC(long handle, GCData data) {
+		final Rectangle eventBounds = new Rectangle(0, 0, size.x, size.y);
+		event.setBounds(eventBounds);
+		final GCData data = new GCData();
+		data.device = this.getDisplay();
+		// critical for drawing without clearing
+		final SkiaGC gc = new SkiaGC(this, SWT.None);
 
-    }
 
-    @Override
-    public Surface getSurface() {
-	return surface;
-    }
+		// here we need the skiaGC init not the gc init...
+		// Note: use GC#setClipping(x,y,width,height) because GC#setClipping(Rectangle)
+		// got broken by bug 446075
+		//		gc.setClipping (eventBounds.x, eventBounds.y, eventBounds.width, eventBounds.height);
+		event.gc = new GCExtension(gc);
+		event.type = SWT.Paint;
+		s.sendPaintEvent(event);
+		gc.dispose();
+		event.gc = null;
+
+		surface.flush();
+
+	}
+
+	@Override
+	public void createSurface(long pointer, Point size, RasterImageInfo info) {
+
+		final var s = getSize();
+		final ColorAlphaType type = info.premule ? ColorAlphaType.PREMUL : ColorAlphaType.UNPREMUL;
+		final ImageInfo imageInfo = ImageInfo.makeN32(size.x, size.y, type);
+
+		this.surface = Surface.makeRasterDirect(imageInfo, pointer, 4 * (size.x));
+
+		if (info.transform) {
+			surface.getCanvas().scale(1, -1);
+			surface.getCanvas().translate(0, -s.y);
+		}
+
+	}
+
+	@Override
+	void preResize(Event e) {
+
+		if (surface != null && !surface.isClosed()) {
+			surface.close();
+		}
+
+	}
+
+	@Override
+	public long internal_new_GC(GCData data) {
+		return 0;
+	}
+
+	@Override
+	public void internal_dispose_GC(long handle, GCData data) {
+
+	}
+
+	@Override
+	public Surface getSurface() {
+		return surface;
+	}
 
 }

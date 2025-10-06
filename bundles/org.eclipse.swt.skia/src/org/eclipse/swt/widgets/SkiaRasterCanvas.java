9d321e479e091cec32f5f4522c0a8507955b10b2
Denis Ungemach <denis.ungemach@sap.com>
2025-10-06 14:43:50 +0200

    SkiaCanvas: Create a SkiaResource for caching information.

---
+++ bundles/org.eclipse.swt.skia/src/org/eclipse/swt/widgets/SkiaRasterCanvas.java
@@ -0,0 +1,15 @@
+package org.eclipse.swt.widgets;
+
+public class SkiaRasterCanvas extends Canvas{
+
+	public SkiaRasterCanvas(Composite parent, int style) {
+		super (parent,  prepare(style));
+		SkiaConfiguration.resetCanvasConfiguration();
+	}
+
+	private static int prepare(int style) {
+		SkiaConfiguration.activateSkiaRaster();
+		return style;
+	}
+
+}

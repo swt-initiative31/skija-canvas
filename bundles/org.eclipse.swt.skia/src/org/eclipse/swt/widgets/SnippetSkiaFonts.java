2504a73d905d5a262da473822da91154d1844a86
Denis Ungemach <denis.ungemach@sap.com>
2025-10-15 13:47:50 +0200

    SkiaCanvas: add snippets for features.

---
+++ bundles/org.eclipse.swt.skia/src/org/eclipse/swt/widgets/SnippetSkiaFonts.java
@@ -0,0 +1,98 @@
+package org.eclipse.swt.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+
+public class SnippetSkiaFonts {
+
+	final static int stepSize = 60;
+
+	public static void main(String[] args) {
+		final Display display = new Display();
+		final Shell shell = new Shell(display, SWT.V_SCROLL |  SWT.SHELL_TRIM);
+		shell.setText("Available Fonts");
+		shell.setSize(800, 600);
+		shell.setLayout(new GridLayout(2,true));
+
+		final Canvas canvas = new Canvas(shell,  SWT.BORDER);
+		final Canvas skiaCanvas = new SkiaRasterCanvas(shell, SWT.BORDER);
+
+		setGridData(canvas, 300);
+		setGridData(skiaCanvas, 300);
+
+
+		final var pl = (PaintListener) event -> {
+			final GC gc = event.gc;
+			final FontData[] fontDataArrayScalable = display.getFontList(null, true);
+			final FontData[] fontDataArrayNotScalable = display.getFontList(null, false);
+			int y = 10 - shell.getVerticalBar().getSelection()   ;
+			for (final FontData fd : fontDataArrayScalable) {
+
+				if( y < -stepSize || y > canvas.getSize().y ) {
+					y += stepSize;
+					continue;
+				}
+
+				final Font font = new Font(display, fd);
+				gc.setFont(font);
+				final String fontName = fd.getName();
+				gc.drawText(fontName, 10, y);
+				y += stepSize;
+				font.dispose();  // Frees system resources
+			}
+
+			for (final FontData fd : fontDataArrayNotScalable) {
+
+				if( y < -stepSize || y > canvas.getSize().y ) {
+					y += stepSize;
+					continue;
+				}
+
+				final Font font = new Font(display, fd);
+				gc.setFont(font);
+				final String fontName = fd.getName();
+				gc.drawText(fontName, 10, y);
+				y += stepSize;
+				font.dispose();  // Frees system resources
+			}
+
+			shell.getVerticalBar().setMaximum(y + shell.getVerticalBar().getSelection());
+		};
+
+		canvas.addPaintListener(pl );
+		skiaCanvas.addPaintListener(pl);
+
+
+		shell.getVerticalBar().addListener(SWT.Selection, e -> {
+
+			canvas.redraw();
+			skiaCanvas.redraw();
+
+		});
+
+
+
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.dispose();
+	}
+
+	private static void setGridData(Control c, int minHeight) {
+		final var g = new GridData();
+		g.grabExcessHorizontalSpace = true;
+		g.grabExcessVerticalSpace = true;
+		g.horizontalAlignment = GridData.FILL;
+		g.verticalAlignment = GridData.FILL;
+		g.minimumHeight = minHeight;
+		c.setLayoutData(g);
+	}
+}

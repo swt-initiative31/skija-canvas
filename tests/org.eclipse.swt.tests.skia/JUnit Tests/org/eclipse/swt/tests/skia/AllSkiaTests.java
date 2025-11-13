58abd5b7ed38151111cb1be189d1957f60f553c1
Denis Ungemach <denis.ungemach@sap.com>
2025-11-13 12:44:33 +0100

    SkiaCanvas: Test plugin
    A test plugin for skia: org.eclipse.swt.tests.skia

---
+++ tests/org.eclipse.swt.tests.skia/JUnit Tests/org/eclipse/swt/tests/skia/AllSkiaTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2025 SAP SE and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * Contributors:
+ *     SAP SE and others - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.tests.skia;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite(failIfNoTests = false)
+@SelectClasses({
+	Test_org_eclipse_swt_widgets_SkiaCanvasCompare.class
+})
+public class AllSkiaTests {
+
+}
---
+++ tests/org.eclipse.swt.tests.skia/JUnit Tests/org/eclipse/swt/tests/skia/SkiaConfiguration.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2025 SAP SE and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * Contributors:
+ *     SAP SE and others - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.tests.skia;
+
+public class SkiaConfiguration {
+
+    public static final int SKIA = 1 << 23;
+
+
+    public static final String RASTER = "raster"; //$NON-NLS-1$
+    public static final String OPENGL = "opengl"; //$NON-NLS-1$
+
+    public static final String SKIA_PROPERTY = "org.eclipse.swt.skia.configuration"; //$NON-NLS-1$
+
+    public static void activateSkiaRaster() {
+	System.setProperty("org.eclipse.swt.skia.configuration", RASTER);
+    }
+
+    public static void activateSkiaGl() {
+	System.setProperty("org.eclipse.swt.skia.configuration", OPENGL);
+    }
+
+    public static void resetCanvasConfiguration() {
+	System.setProperty("org.eclipse.swt.skia.configuration", "");
+    }
+
+}

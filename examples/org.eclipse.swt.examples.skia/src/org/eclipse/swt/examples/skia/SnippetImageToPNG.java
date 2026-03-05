fe5150a86490918f356e9cca7c33fdcb25bd91d3
Denis Ungemach <denis.ungemach@sap.com>
2026-03-05 15:16:19 +0100

    SkiaGC: better performance, less interpolation.

--- examples/org.eclipse.swt.examples.skia/src/org/eclipse/swt/examples/skia/SnippetImageToPNG.java
+++ examples/org.eclipse.swt.examples.skia/src/org/eclipse/swt/examples/skia/SnippetImageToPNG.java
@@ -12,31 +12,34 @@ import org.eclipse.swt.widgets.Display;
 
 public class SnippetImageToPNG {
 
-	final static boolean DELETE_OUTPUT_DIR = false;
-	
+	final static boolean DELETE_OUTPUT_DIR = true;
+
+	record ImageInfo(String name, Image image) {
+	}
+
 	record FormatInfo(String format, int type) {
 	}
 
+	
 	private final static List<FormatInfo> formats = List.of(
+//			new FormatInfo(".gif", SWT.IMAGE_GIF), // SWT does not support saving GIFs for these images
+//			new FormatInfo(".ico", SWT.IMAGE_ICO), // SWT does not support saving ICOs for these images
 			new FormatInfo(".bmp", SWT.IMAGE_BMP),
-			new FormatInfo(".gif", SWT.IMAGE_GIF),
-			new FormatInfo(".ico", SWT.IMAGE_ICO),
 			new FormatInfo(".jpef", SWT.IMAGE_JPEG),
 			new FormatInfo(".png", SWT.IMAGE_PNG)
 	);
 	
 
 	private static final String IMAGES_PATH = "images";
-	private final static ArrayList<Image> swtImages = new ArrayList<Image>();
+	private final static ArrayList<ImageInfo> swtImages = new ArrayList<ImageInfo>();
 
 	public static void main(String[] args) {
 		Display display = new Display();
 
 		// Load images
-
-		swtImages.add(Display.getDefault().getSystemImage(SWT.ICON_QUESTION));
-		swtImages.add(Display.getDefault().getSystemImage(SWT.ICON_ERROR));
-		swtImages.add(Display.getDefault().getSystemImage(SWT.ICON_INFORMATION));
+		swtImages.add(new ImageInfo("question", Display.getDefault().getSystemImage(SWT.ICON_QUESTION)));
+		swtImages.add(new ImageInfo("error", Display.getDefault().getSystemImage(SWT.ICON_ERROR)));
+		swtImages.add(new ImageInfo("info", Display.getDefault().getSystemImage(SWT.ICON_INFORMATION)));
 
 		List<File> imageFiles = new ArrayList<>();
 		// Use working directory reliably
@@ -47,7 +50,7 @@ public class SnippetImageToPNG {
 				if (name.endsWith(".png") || name.endsWith(".jpg") || name.endsWith(".jpeg") || name.endsWith(".gif")
 						|| name.endsWith(".bmp") || name.endsWith(".ico")) {
 					try {
-						swtImages.add(new Image(display, f.getAbsolutePath()));
+						swtImages.add(new ImageInfo(name, new Image(display, f.getAbsolutePath())));
 						imageFiles.add(f);
 					} catch (Exception e) {
 						// skip invalid image
@@ -56,7 +59,6 @@ public class SnippetImageToPNG {
 			}
 		}
 
-		int i = 0;
 		File f = new File("./out");
 
 		if (f.exists()) {
@@ -65,23 +67,27 @@ public class SnippetImageToPNG {
 			}
 		}
 		f.mkdirs();
-		
-		for(FormatInfo format : formats) {
-			for (Image img : swtImages) {
+
+		for (FormatInfo format : formats) {
+			System.out.println(format.format + " (" + format.type + "):");
+			int i = 0;
+			for (ImageInfo img : swtImages) {
+
+//				printHasAlpha(img, "original image " + i);
+
 				ImageLoader loader = new ImageLoader();
-				loader.data = new ImageData[] { img.getImageData() };
-				String outPath = "./out/out" + i + ".png";
-				loader.save(outPath, SWT.IMAGE_PNG);
-				// Transparenzprüfung
-				ImageData pngData = new ImageLoader().load(outPath)[0];
-				boolean hasAlpha = pngData.alphaData != null || pngData.alpha != -1;
-				System.out.println("PNG: " + outPath + " hasAlpha=" + hasAlpha + " alphaData="
-						+ (pngData.alphaData != null ? pngData.alphaData.length : "null") + " alpha=" + pngData.alpha);
+				loader.data = new ImageData[] { img.image.getImageData() };
+				String outPath = "./out/out" + i + format.format;
+				loader.save(outPath, format.type);
+
+				if (format.type == SWT.IMAGE_PNG) {
+					printHasAlpha(outPath);
+				}
+
 				i++;
 			}
-			
-		}
 
+		}
 
 		if (DELETE_OUTPUT_DIR) {
 			for (File oldFile : f.listFiles()) {
@@ -91,7 +97,22 @@ public class SnippetImageToPNG {
 
 		}
 	}
-	
 
-	
+//	private static void printHasAlpha(ImageInfo img, String outPath) {
+//		// Transparenzprüfung
+//		ImageData pngData = img.image.getImageData();
+//		boolean hasAlpha = pngData.alphaData != null || pngData.alpha != -1;
+//		System.out.println("Path: " + img.name + " hasAlpha=" + hasAlpha + " alphaData="
+//				+ (pngData.alphaData != null ? pngData.alphaData.length : "null") + " alpha=" + pngData.alpha);
+//
+//	}
+
+	private static void printHasAlpha(String outPath) {
+		// Transparenzprüfung
+		ImageData pngData = new ImageLoader().load(outPath)[0];
+		boolean hasAlpha = pngData.alphaData != null || pngData.alpha != -1;
+		System.out.println("Path: " + outPath + " hasAlpha=" + hasAlpha + " alphaData="
+				+ (pngData.alphaData != null ? pngData.alphaData.length : "null") + " alpha=" + pngData.alpha);
+	}
+
 }

9dd39c7cc18df86d0904bb9f71bba01c1a743c37
Denis Ungemach <denis.ungemach@sap.com>
2026-03-16 13:49:21 +0100

    Skia: add copyright messages.

--- bundles/org.eclipse.swt.skia/src/org/eclipse/swt/internal/graphics/SkiaFontMetrics.java
+++ bundles/org.eclipse.swt.skia/src/org/eclipse/swt/internal/graphics/SkiaFontMetrics.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2025 SAP SE and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -7,9 +7,6 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.swt.internal.graphics;
 
